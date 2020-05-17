42cc9486227d01becf1c1d3351a39d01ba897d76
Your Name <you@example.com>
2020-05-17 00:20:49 -0700

    _2020_0517__0020am

--- src7/m3_powerAndSpeedCalc.v
+++ src7/m3_powerAndSpeedCalc.v
@@ -21,7 +21,27 @@ input   wire                m3powerDECi	;
 input wire                  clkI        ;
 input wire                  nRstI       ;
 
+/*
+*
+* 1Mhz clock, 1us clock period; 
+* max sine wave freqence is 25kHz == 40us = 40 clk
+* min sine wave freqence is 0.25Hz == 4,000,000 us = 4,000,000 clk == 22'd4000000
+*
+* about the increase speed calc :
+* LLL={3/2, 5/4, 9/8, 17/16, 33/32} ; Fmin=40 ; Fmax=4000000 ;
+* Log[ LLL, (Fmax/Fmin) ] == {28.3944, 51.5943, 97.7469, 189.905, 374.141}
+* so, if the inc rate range from 1/2 to 1/32 , the required time from 28 to 374
+*/
+`ifdef    simulating
+//    +define+simulating , to reduce the VCS debug time.
+`define   clkPeriodMax      22'd400
+`else
+`define   clkPeriodMax      22'd4000000
+`endif
+`define   clkPeriodMin      22'd40
+
 reg          [3:0]          step        ;
+
 
 always @( posedge clkI or negedge nRstI ) begin
     if ( ! nRstI ) begin
