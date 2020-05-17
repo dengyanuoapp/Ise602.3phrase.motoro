1b5a798762fdd764fa4ea20886b91dadc7778876
Your Name <you@example.com>
2020-05-16 23:23:54 -0700

    20200516_2323PM_

--- src7/m3_powerAndSpeedCalc.v
+++ src7/m3_powerAndSpeedCalc.v
@@ -25,12 +25,16 @@ reg          [3:0]          step        ;
 
 always @( posedge clkI or negedge nRstI ) begin
     if ( ! nRstI ) begin
-        step        <= 4'd0                  ;
+        //step        <= 4'd0                  ;
+        step            <= 4'hF                  ;
     end
     else begin
-        if ( 1 == 1 ) begin
-            if ( step == 4'd12 ) begin
-                step    <= 4'd1              ;
+        if ( m3startI == 1'b0 ) begin
+            step        <= 4'hF                  ;
+        end
+        else begin
+            if ( step == 4'd11 ) begin
+                step    <= 4'd0              ;
             end
             else begin
                 step    <= step    + 4'd1    ;
