f3d38c85829d1a947c2efe2829f16cbafc08ab87
dyn <y@e>
2020-05-21 17:45:57 -0700

    20200521_1745PM_

--- src9/motor602_rtl_top.def.inc.v
+++ src9/motor602_rtl_top.def.inc.v
@@ -12,6 +12,17 @@
 //    `endif
 //`endif
 
+    /*
+    *
+    * 1Mhz clock, 1us clock period; 
+    * max sine wave freqence is 25kHz == 40us = 40 clk
+    * min sine wave freqence is 0.25Hz == 4,000,000 us = 4,000,000 clk == 22'd4000000
+    *
+    * about the increase speed calc :
+    * LLL={3/2, 5/4, 9/8, 17/16, 33/32} ; Fmin=40 ; Fmax=4000000 ;
+    * Log[ LLL, (Fmax/Fmin) ] == {28.3944, 51.5943, 97.7469, 189.905, 374.141}
+    * so, if the inc rate range from 1/2 to 1/32 , the required time from 28 to 374
+    */
 
 `ifdef    simulating
     //    +define+simulating , to reduce the VCS debug time.
@@ -22,3 +33,12 @@
 `else
     `define   eachSlicePeriodMax      22'd4000000
 `endif
+
+    /*
+    * Total Stotal    == (1.0 * powerLevel ) * len
+    * up :   SAup     == ((0.5773502692 + 1) / 2 * powerLevel) * len == 0.7886751346 * Stotal
+    *       Sup(xx_)  =  (0.5773502692 + ((( nowStepEndEdge / len ) * ( 1 - 0.5773502692)) / 2)) * Stotal
+    * down : SAdown   == ((1 + 0.8660254038) / 2 * powerLevel) * len == 0.9330127019 * Stotal
+    *     Sdown(xx_)  =  (1 - ( nowStepEndEdge / len ) * ( 1 - 0.8660254038) ) / 2 * Stotal
+    */
+
