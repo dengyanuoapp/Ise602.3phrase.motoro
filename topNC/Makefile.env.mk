054629f64b5908e2734297c71d76f2c29b854255
dyn <y@e>
2020-05-26 21:47:46 -0700

    20200526_2147PM_

---
+++ topNC/Makefile.env.mk
@@ -0,0 +1,11 @@
+topModule:=motoro301_tb
+DEV01:=xc3s500e-pq208-4
+
+CFGmakeRun:=Makefile.run.cadenceNC.mk
+
+#ttUCF:=ip2uart_top.ucf
+
+#excludeSIMfileLists:= srcSYN/src9/motoro301_top.v
+
+NCdefine:= +define+simulating=1
+
