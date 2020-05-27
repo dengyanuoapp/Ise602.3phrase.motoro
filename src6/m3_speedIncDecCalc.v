a9c1a84c77a5ed9b2b2422817d440f71f15ec5f3
dyn <y@e>
2020-05-26 17:17:04 -0700

    _2020_0526__1717pm

--- src6/m3_speedIncDecCalc.v
+++ src6/m3_speedIncDecCalc.v
@@ -35,6 +35,103 @@ module m3_speedIncDecCalc (
     always @( posedge clkI or negedge nRstI ) begin
         if ( ! nRstI ) begin
             dstRoundLenO                            <= `eachSlicePeriodMax        ;
+        end
+        else begin
+            if ( workingI == 1'b0 ) begin
+                dstRoundLenO                        <= `eachSlicePeriodMax        ;
+            end
+            else begin
+                if ( roundCnt1round == 4'd0 ) begin // 1/16 --> inc freq 6.25%
+                    if ( m3speedINCi == 1'b1 ) begin
+                        if ( dstRoundLenO- dstRoundLenO[31:4] > `clkPeriodMin ) begin // reach max freq(min period)
+                            dstRoundLenO       <= dstRoundLenO- dstRoundLenO[31:4] ;
+                        end
+                        else begin
+                            dstRoundLenO       <= `clkPeriodMin        ;
+                        end
+                    end
+                    else begin
+                        if ( m3speedDECi == 1'b1 ) begin
+                            if ( dstRoundLenO + dstRoundLenO[31:4] > `eachSlicePeriodMax ) begin // reach min freq(max period)
+                                dstRoundLenO   <= `eachSlicePeriodMax        ;
+                            end
+                            else begin
+                                dstRoundLenO   <= dstRoundLenO+ dstRoundLenO[31:4] ; 
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    always @( posedge clkI or negedge nRstI ) begin
+        if ( ! nRstI ) begin
+            dstRoundLenO                            <= `eachSlicePeriodMax        ;
+            roundCnt1round                          <= `roundMax            ;
+            roundLast                               <= 1'b0                 ;
+        end
+        else begin
+            if ( workingI == 1'b0 ) begin
+                dstRoundLenO                        <= `eachSlicePeriodMax        ;
+                roundCnt1round                      <= `roundMax            ;
+                roundLast                           <= 1'b0                 ;
+            end
+            else begin
+                if ( nextCalc_1I  == 1'b1 ) begin
+                    if ( m3speedINCi == 1'b1 ) begin
+                        if ( roundLast == 1'b0 ) begin // ok , it is INCing
+                            if ( roundCnt1round == 4'd0 ) begin // 1/16 --> inc freq 6.25%
+                                roundCnt1round      <= `roundMax            ;
+                                if ( dstRoundLenO- dstRoundLenO[31:4] > `clkPeriodMin ) begin // reach max freq(min period)
+                                    dstRoundLenO       <= dstRoundLenO- dstRoundLenO[31:4] ;
+                                end
+                                else begin
+                                    dstRoundLenO       <= `clkPeriodMin        ;
+                                end
+                            end
+                            else begin
+                                roundCnt1round      <= roundCnt1round - 4'd1      ;
+                            end
+                        end
+                        else begin // transmit from DECing to INCing. reset counter.
+                            roundCnt1round          <= `roundMax            ; 
+                            roundLast               <= 1'b0                 ;
+                        end
+                    end 
+                    else begin
+                        if ( m3speedDECi == 1'b1 ) begin
+                            if ( roundLast == 1'b1 ) begin // ok , it is DECing
+                                if ( roundCnt1round == 4'd0 ) begin // 1/16 --> inc 6.25%, dec freq 6.25%
+                                    roundCnt1round  <= `roundMax            ;
+                                    if ( dstRoundLenO + dstRoundLenO[31:4] > `eachSlicePeriodMax ) begin // reach min freq(max period)
+                                        dstRoundLenO   <= `eachSlicePeriodMax        ;
+                                    end
+                                    else begin
+                                        dstRoundLenO   <= dstRoundLenO+ dstRoundLenO[31:4] ; 
+                                    end
+                                end
+                                else begin
+                                    roundCnt1round  <= roundCnt1round - 4'd1      ;
+                                end
+                            end
+                            else begin // transmit from INCing to DECing . reset counter.
+                                roundCnt1round      <= `roundMax            ; 
+                                roundLast           <= 1'b1                 ; 
+                            end
+                        end 
+                        else begin // not INCing , not DECing. reset counter.
+                            roundCnt1round          <= `roundMax            ; 
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    always @( posedge clkI or negedge nRstI ) begin
+        if ( ! nRstI ) begin
+            dstRoundLenO                            <= `eachSlicePeriodMax        ;
             roundCnt1round                          <= `roundMax            ;
             roundLast                               <= 1'b0                 ;
         end
